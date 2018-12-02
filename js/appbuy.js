eff7358599a8ac1ae7e1be664ea16486e45d6788
Dolores Melgar <doloresmelgarfreire@gmail.com>
2018-12-02 23:01:34 +0000

    JQuery and Social Buttons
    JQuery file added in js folder
    Facebook button added in sell.html
    changes from "Share it!" to "Facebook" when clicked

---
+++ js/appbuy.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function(){
+  $('#n1').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n1').parent().append(shareit_change);
+    $('#n1').remove();
+  });
+
+  $('.n2').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n2').parent().append(shareit_change);
+    $('#n2').remove();
+  });
+
+  $('#n3').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n3').parent().append(shareit_change);
+    $('#n3').remove();
+  });
+
+  $('#n4').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n4').parent().append(shareit_change);
+    $('#n4').remove();
+  });
+
+  $('#n5').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n5').parent().append(shareit_change);
+    $('#n5').remove();
+  });
+  $('#n6').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    $('#n6').parent().append(shareit_change);
+    $('#n6').remove();
+  });
+});
+
+
+//$('#share_btn').find('#1').on('click',function(){
+//  var shareit = $();
+//  $('#share_btn').find('#1').parent().append(shareit);
+//  $('#share_btn').remove();
