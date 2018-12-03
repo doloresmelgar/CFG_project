77903b9fcb8794952a93cbb737dc0659bbf794d6
Dolores Melgar <doloresmelgarfreire@gmail.com>
2018-12-03 09:17:32 +0000

    Fixing facebook buttons
    Facebook buttons in buy page looking better
    issue with product2 fixed

--- js/appbuy.js
+++ js/appbuy.js
@@ -1,35 +1,35 @@
 jQuery(document).ready(function(){
   $('#n1').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n1').parent().append(shareit_change);
     $('#n1').remove();
   });
 
-  $('.n2').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+  $('#n2').on('click',function(){
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n2').parent().append(shareit_change);
     $('#n2').remove();
   });
 
   $('#n3').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n3').parent().append(shareit_change);
     $('#n3').remove();
   });
 
   $('#n4').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n4').parent().append(shareit_change);
     $('#n4').remove();
   });
 
   $('#n5').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n5').parent().append(shareit_change);
     $('#n5').remove();
   });
   $('#n6').on('click',function(){
-    var shareit_change = $('<button class="fb_btn"> Facebook </button>');
+    var shareit_change = $('<button class="fb_btn"> &nbsp;Facebook&nbsp; </button>');
     $('#n6').parent().append(shareit_change);
     $('#n6').remove();
   });
